4bda02ef8b667db2f3d9a6da5f89e7229844a160
113295634 <113295634@qq.com>
2020-06-12 14:50:31 +0800

    Neural Work

---
+++ Neural_work/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"E:\\Dignal Image\\neural network1\\text.mp4"		
+#define OPENPOSE_VIDEO		"E:\\Dignal Image\\neural network1\\text.mp4"		
+
+//#define INPUT_METHOD									//0: image 1:video 2:camera
